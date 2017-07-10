16e29325b87cc62cbc04eb92caee6b6e5f0e0fff
Ed <edoisa6@gmail.com>
2017-07-09 22:48:41 -0700

    Structure Updated

---
+++ ControlPanelUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class ControlPanel {
+	private JPanel panel1;
+	
+	
+	
+}
---
+++ UserGroup.java
@@ -0,0 +1,7 @@
+package PACKAGE_NAME;
+
+/**
+ * Created by Ed on 7/9/2017.
+ */
+public class UserGroup {
+}
