b2a6c240512c27d4fd33e2cb6ecccc0568b14622
Ed <edoisa6@gmail.com>
2017-07-18 23:26:57 -0700

    Restructured

---
+++ AdminControlPanelUI.java
@@ -0,0 +1,41 @@
+/*
+6. A few analysis features are needed in the admin control panel:
+	1) output the total number of users;
+	2) output the total number of groups;
+	3) output the total number of Tweet messages in all the users’ news feed;
+	4) output the percentage of the positive Tweet messages in all the users’
+	news feed (the message containing positive words, such as good, great,
+	excellent, etc.) Free free to decide the positive words.
+	
+	** UI **
+2.  The Admin Control Panel is the main UI you will see by running the program.
+	This should be the entrance to the program.
+3.  You can add users/groups with Buttons and TextAreas. Of course, the TreeView
+	should be updated as well whenever new users/groups are being added.
+4.  For groups, you should display them with a different notation (maybe a folder
+	icon or something else just to distinguish it from the users).
+5.  Deleting users/groups is not required.
+6.  Clicking on the 4 buttons at the bottom-right will output the correspondent
+	information. You can simply popup a dialog to display the value, or use some
+	other widgets.
+7.  When you select a user in the tree, clicking on the Open User View button will
+	open the User View as shown in Figure 2. You can open multiple User Views for
+	different users.
+8.  In the User View, it will display the current users you are following in a
+	ListView (followings). You can add new users to follow by using the TextArea
+	and Button. Unfollow is not required. Displaying your followers is not required.
+9.  The User View also shows the current news feed list for this user in a ListView.
+10. You can post a new Tweet with the TextArea and Button. Once you click the Post
+	button. It will add the message to all your followers’ news feed list, as well
+	as your own news feed list.
+11. Whenever a new message is posted, all the followers’ news feed list view should
+	be updated and refreshed automatically.
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ControlPanelUI extends JFrame {
+
+
+}
--- UserViewUI.java
+++ UserViewUI.java
@@ -1,4 +1,9 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class UserViewUI {
+public class UserViewUI extends JFrame {
+
 
 }
